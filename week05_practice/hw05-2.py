afea2566003586926d234ff27c77a4c8872cb14d
Kim-Jiwan <wldhks5819@gmail.com>
2022-03-30 21:30:18 +0900

    first commit

---
+++ week05_practice/hw05-2.py
@@ -0,0 +1,9 @@
+a, b, c = map(int, input().split())
+
+while a != 0 and b != 0 and c != 0:
+    if a ** 2 == b ** 2 + c ** 2 or b ** 2 == a ** 2 + c ** 2 or c ** 2 == a ** 2 + b ** 2:
+        print("right")
+    else:
+        print("wrong")
+    
+    a, b, c = map(int, input().split())
